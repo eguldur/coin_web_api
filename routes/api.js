853b07b4f0b973c22732350839e97153f135ddb9
eguldur <erdem@erdemguldur.com>
2018-01-20 11:34:42 +0300

    as

--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    mongoose.connect('mongodb://localhost:27017/nodedemo')
-        .then(()=> { res.send(`Succesfully Connected to the Mongodb Database`)})
-        .catch(()=> { res.send(`Error Connecting to the Mongodb Database`)});
+    res.send(`Succesfully Connected to the Mongodb Database`);
+
 });
 
 module.exports = router;
